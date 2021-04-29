569fe563a5d8280b098b7bf666cbe6a17cadc882
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-29 23:13:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 end
+
+def countdown_with_sleep(number)
+  def countdown(number)
+    while number > 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+    "HAPPY NEW YEAR!"
+  end
+  end
